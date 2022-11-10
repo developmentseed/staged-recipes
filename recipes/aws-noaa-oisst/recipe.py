579af83b8b3bd74d1b8e14299975672aa4ab5a50
kathrynberger <kathryn@developmentseed.org>
2022-11-10 14:24:24 +0000

    recipe after run

--- recipes/aws-noaa-oisst/recipe.py
+++ recipes/aws-noaa-oisst/recipe.py
@@ -13,12 +13,9 @@ def format_function(time):
         "s3://noaa-cdr-sea-surface-temp-optimum-interpolation-pds/data"
         "/v2.1/avhrr/{day:%Y%m}/oisst-avhrr-v02r01.{day:%Y%m%d}.nc"
     )
-
     return input_url_pattern.format(day=day)
 
 
 dates = pd.date_range(start_date, "2022-11-08", freq="D")
 pattern = FilePattern(format_function, ConcatDim("time", range(len(dates)), 1))
-print(pattern)
 recipe = XarrayZarrRecipe(pattern, inputs_per_chunk=20, cache_inputs=True)
-print(recipe)
