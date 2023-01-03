70d8fad5335a785cfc2bf306fb5e444129c38008
kathrynberger <kathryn@developmentseed.org>
2023-01-03 16:01:06 +0000

    iterate pattern by sensor

--- recipes/aws-noaa-fcdr-micro-brit-tmp/recipe.py
+++ recipes/aws-noaa-fcdr-micro-brit-tmp/recipe.py
@@ -1,4 +1,4 @@
-import glob
+# import glob
 import os
 from os.path import join
 
@@ -7,11 +7,6 @@ import s3fs
 from pangeo_forge_recipes.patterns import pattern_from_file_sequence
 
 # from pangeo_forge_recipes.recipes.reference_hdf_zarr import HDFReferenceRecipe
-
-url_base = 'noaa-cdr-microwave-brit-temp-pds/data/'
-
-file_list = []
-fs = s3fs.S3FileSystem(anon=True)
 
 
 def is_nc(x):
@@ -22,23 +17,36 @@ def add_s3(x):
     return 's3://' + x
 
 
-years_folders = fs.ls(join(url_base))
-years = list(map(lambda x: os.path.basename(x), years_folders))
-
-sensor_list = ['N15', 'N16', 'N17', 'N18', 'AQUA', 'M02']
+# sensor_list = ['N15', 'N16', 'N17', 'N18', 'AQUA', 'M02']
 
 # psuedo code:
 # iterate through sensor list, if sensor (e.g., N15) is in file_list
 # iterate through year in years and create separate archive for each sensor
 
-# work-in-progess implementing pseudo code ab
+# work-in-progess implementing pseudo code above
+url_base = 'noaa-cdr-microwave-brit-temp-pds/data/'
+fs = s3fs.S3FileSystem(anon=True)
+
+years_folders = fs.ls(join(url_base))
+years = list(map(lambda x: os.path.basename(x), years_folders))
+
+file_list = []
 for year in years:
     file_list += sorted(filter(is_nc, map(add_s3, fs.ls(join(url_base, str(year)), detail=False))))
-pattern = pattern_from_file_sequence(file_list, 'time', nitems_per_file=1)
-for item in sensor_list:
-    for name in glob.glob('*.nc'):
-        if item in name:
-            print(file_list)
+    for file in file_list:
+        sensor = file.split('_')[4]
+        for sensor in file_list:
+            pattern = pattern_from_file_sequence(file_list, 'time', nitems_per_file=1)
+            print(pattern)
+
+
+#     pattern = pattern_from_file_sequence(file_list, 'time', nitems_per_file=1)
+
+
+# for item in sensor_list:
+#     for name in glob.glob('*.nc'):
+#         if item in name:
+#             print(file_list)
 
 # print(pattern)
 # recipe = HDFReferenceRecipe(pattern, netcdf_storage_options={'anon': True})
