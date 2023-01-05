cf279c2259ae63ff4e8236f112ffa022291d99b8
kathrynberger <kathryn@developmentseed.org>
2023-01-05 14:58:58 +0000

    try with boto3

--- recipes/aws-noaa-fcdr-micro-brit-tmp/recipe.py
+++ recipes/aws-noaa-fcdr-micro-brit-tmp/recipe.py
@@ -1,11 +1,12 @@
-# import glob
-import os
-from os.path import join
+# import csv
+# import os
+# from os.path import join
 
-import s3fs
+import boto3
+
+# import s3fs
 
 # from pangeo_forge_recipes.patterns import pattern_from_file_sequence
-
 # from pangeo_forge_recipes.recipes.reference_hdf_zarr import HDFReferenceRecipe
 
 
@@ -17,53 +18,54 @@ def add_s3(x):
     return 's3://' + x
 
 
+client = boto3.client('s3')
+bucket_name = 'noaa-cdr-microwave-brit-temp-pds'
+
+paginator = client.get_paginator('list_objects_v2')
+response_iterator = paginator.paginate(
+    Bucket=bucket_name,
+    Prefix='data',
+)
+
 # sensor_list = ['N15', 'N16', 'N17', 'N18', 'AQUA', 'M02']
 
-# work-in-progess implementing pseudo code above
-url_base = 'noaa-cdr-microwave-brit-temp-pds/data/'
-fs = s3fs.S3FileSystem(anon=True)
+for result in response_iterator:
+    contents = result.get('Contents', [])
+    # can implement filter here
+    for f in contents:
+        key = f['Key']
+        print(f's3://{bucket_name}/{key}')
 
-years_folders = fs.ls(join(url_base))
-years = list(map(lambda x: os.path.basename(x), years_folders))
-
-file_list = []
-
-# WiP
+# f["Key"].split('_')[4] # gets us satellite value
 
 
-def add_sensor(sensor_name):
-    sensors_list = fs.find(url_base)
-    for sensor in sensors_list:
-        sensor_name = sensor.split('_')[4]
-        return 'NESDIS-STAR_FCDR-GRID_AMSU-A_V01R00_' + sensor_name
+# work-in-progess old method
+
+# url_base = 'noaa-cdr-microwave-brit-temp-pds/data/'
+# fs = s3fs.S3FileSystem(anon=True)
+
+# years_folders = fs.ls(join(url_base))
+# years = list(map(lambda x: os.path.basename(x), years_folders))
+
+# file_list = []
+
+# def add_sensor(sensor_name):
+#     sensors_list = fs.find(url_base)
+#     for sensor in sensors_list:
+#         sensor_name = sensor.split('_')[4]
+#         return 'NESDIS-STAR_FCDR-GRID_AMSU-A_V01R00_' + sensor_name
 
 
-# To Do:
-# add sensor to URL,
+# for year in years:
+#   file_list += sorted(filter(is_nc, map(add_s3, fs.ls(join(url_base, str(year)), detail=False))))
+
+#  # To Do:
+#  # add add_sensor to URL (to retrieve only those with GRID in file name, iterate thru all sensors)
+
+#     pattern = pattern_from_file_sequence(file_list, 'time', nitems_per_file=1)
+#     print(pattern)
 
 # psuedo code:
 # iterate through sensor list, if sensor (e.g., N15) is in file_list
 # iterate through year in years and create separate archive for each sensor
-
-
-# for sensor in sensors:
-#    file_list += sorted(filter(is_nc, map(add_s3, fs.ls(join(url_base, str(year)), detail=False))))
-#    for file in file_list:
-#        sensor = file.split('_')[4]
-#        for sensor in file_list:
-#            pattern = pattern_from_file_sequence(file_list, 'time', nitems_per_file=1)
-#            print(pattern)
-
-# file_list = []
-# for year in years:
-#    file_list += sorted(filter(is_nc, map(add_s3, fs.ls(join(url_base, str(year)), detail=False))))
-#    for file in file_list:
-#        sensor = file.split('_')[4]
-#        for sensor in file_list:
-#            pattern = pattern_from_file_sequence(file_list, 'time', nitems_per_file=1)
-#            print(pattern)
-
-
-#     pattern = pattern_from_file_sequence(file_list, 'time', nitems_per_file=1)
-
-# recipe = HDFReferenceRecipe(pattern, netcdf_storage_options={'anon': True})
+# want to ignore files that don't include GRID in filename
