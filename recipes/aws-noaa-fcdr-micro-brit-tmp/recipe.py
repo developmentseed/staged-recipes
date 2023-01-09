7faafcca1c7c13eab09b7672402c84f50c9213d2
kathrynberger <kathryn@developmentseed.org>
2023-01-09 15:52:59 +0000

    iterate with sensor as if statement

--- recipes/aws-noaa-fcdr-micro-brit-tmp/recipe.py
+++ recipes/aws-noaa-fcdr-micro-brit-tmp/recipe.py
@@ -4,9 +4,11 @@
 
 import boto3
 
+# from pangeo_forge_recipes.patterns import pattern_from_file_sequence
+
 # import s3fs
 
-# from pangeo_forge_recipes.patterns import pattern_from_file_sequence
+
 # from pangeo_forge_recipes.recipes.reference_hdf_zarr import HDFReferenceRecipe
 
 
@@ -22,21 +24,40 @@ client = boto3.client('s3')
 bucket_name = 'noaa-cdr-microwave-brit-temp-pds'
 
 paginator = client.get_paginator('list_objects_v2')
+
+# 'list_objects_v2': Returns some or all (up to 1000) of the objects in a bucket
+# this could be a limiting factor
+
 response_iterator = paginator.paginate(
     Bucket=bucket_name,
     Prefix='data',
 )
 
 # sensor_list = ['N15', 'N16', 'N17', 'N18', 'AQUA', 'M02']
+# must also ignore files that don't include GRID in filename
 
+
+# WORKS
 for result in response_iterator:
     contents = result.get('Contents', [])
     # can implement filter here
     for f in contents:
         key = f['Key']
-        print(f's3://{bucket_name}/{key}')
+        # print(f's3://{bucket_name}/{key}')
 
-# f["Key"].split('_')[4] # gets us satellite value
+        if f['Key'].split('_')[4] == 'N15':
+            # years = f["Key"].split('/')[1]
+            # print(years.count('2022'))
+
+            matching_files = [f for f in contents]
+            print(matching_files)
+
+            # pattern_N15 = pattern_from_file_sequence(matching_files, 'time')
+            # print(pattern_N15)
+
+
+# pattern = pattern_from_file_sequence(file_list, 'time', nitems_per_file=1)
+# recipe = HDFReferenceRecipe(pattern, netcdf_storage_options={'anon': True})
 
 
 # work-in-progess old method
