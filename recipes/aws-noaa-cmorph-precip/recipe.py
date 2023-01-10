341c080d745fe9df49181ec5ec88939dda98d71c
Rub21 <ruben@developmentseed.org>
2023-01-10 13:40:41 -0500

    Update date range

--- recipes/aws-noaa-cmorph-precip/recipe.py
+++ recipes/aws-noaa-cmorph-precip/recipe.py
@@ -8,7 +8,7 @@ from dask.diagnostics import ProgressBar
 url_base = "s3://noaa-cdr-precip-cmorph-pds/data/daily/0.25deg/"
 file_list = []
 fs = s3fs.S3FileSystem(anon=True)
-dates = pd.date_range("1998", "2022", freq="A")
+dates = pd.date_range("1998", "2022", freq="M")
 
 
 def is_nc(x):
@@ -20,12 +20,11 @@ def add_s3(x):
 
 
 file_list = []
-for year in dates.year:
-    for month in range(1,13):
-        month_str=str(month).zfill(2)
-        date_path = f"{str(year)}/{month_str}/"
-        file_list += [add_s3(x) for x in fs.ls(join(url_base, date_path )) if is_nc(x)]
+for d in dates:
+    date_path = f"{str(d.year)}/{str(d.month).zfill(2)}/"
+    file_list += [add_s3(x) for x in fs.ls(join(url_base, date_path)) if is_nc(x)]
 file_list = sorted(file_list)
+
 
 pattern = pattern_from_file_sequence(file_list, "time", nitems_per_file=1)
 recipe = HDFReferenceRecipe(pattern, netcdf_storage_options={"anon": True})
